95761afb4d4f3b7e6e4e83c2e4dda870eeea50e7
Willem Toorop <willem@toorop.net>
2024-10-09 15:58:07 +0200

    Berichtje fix

--- berichtje.php
+++ berichtje.php
@@ -1,5 +1,8 @@
 <?php 
-  if (mail( "astrid-en-willem@tools2c.nl"
+  if (trim($_POST["Naam"]) == "" && trim($_POST["E-mail"]) == ""
+  &&  trim($_POST["Berichtje"]) == "") {
+    print("<p><i class=\"fas fa-exclamation-triangle w3-text-red\"></i> Geen bericht ontvangen. Probeert u het nog eens.</p>");
+  } else if (mail( "astrid-en-willem@tools2c.nl"
           , "Tools2C msg van " . $_POST["Naam"] . " <" . $_POST["E-mail"] . ">"
           , "Van    : " . $_POST["Naam"] . "\r\n"
           . "E-mail : " . $_POST["E-mail"] . "\r\n"
